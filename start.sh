2b27774b3a4c9a576d3aafe71ecfc82390d72d15
code.vvip <wxm4ever@gmail.com>
2012-06-27 13:24:47 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash - 
+set -o nounset
+
+# update the source 
+apt-get update
+# update the system to the current newest
+update-manager -d
+apt-get install aptitude
+aptitude update
+aptitude install vim vim-gtk vim-gnome git
+
+# upgrade the system
+aptitude upgrade
