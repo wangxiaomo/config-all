3d42ae381ddb17777ac0dc96545509b22737bec8
xiaomo <code.vvip@gmail.com>
2012-07-02 14:39:28 +0800

    change

--- run.pl
+++ run.pl
@@ -6,3 +6,4 @@ use warnings;
 `rm -f ~/.vimrc && cp vimrc ~/.vimrc`;
 `rm -f ~/.tmux.conf && cp tmux.conf ~/.tmux.conf`;
 `rm -f ~/.bashrc && cp bash.bashrc ~/.bashrc`;
+`rm -f ~/.bash_profile && cp bash.bash_profile ~/.bash_profile`;
